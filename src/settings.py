5d34b3d67a3105814eb075b4cb76a34f3994d389
wartinich <vasilenko.arhip@gmail.com>
2023-03-17 18:59:24 +0200

    Add the ability to read env for settings

--- src/settings.py
+++ src/settings.py
@@ -1,10 +1,15 @@
+import os
+
+from dotenv import load_dotenv
 from pydantic import BaseConfig
+
+load_dotenv()
 
 
 class Setting(BaseConfig):
-    DATABASE_URL: str = "sqlite:///./portal.db"
-    JWT_SECRET: str = "secret"
-    JWT_ALGORITHM: str = "HS256"
+    DATABASE_URL: str = os.environ.get("DATABASE_URL")
+    JWT_SECRET: str = os.environ.get("JWT_SECRET")
+    JWT_ALGORITHM: str = os.environ.get("JWT_ALGORITHM")
 
 
 settings = Setting()
