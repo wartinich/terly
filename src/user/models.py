c0f933a9c49db1eafafd8317e83d36600619da0b
wartinich <vasilenko.arhip@gmail.com>
2023-03-17 11:34:07 +0200

    Add model for User

---
+++ src/user/models.py
@@ -0,0 +1,18 @@
+import time
+
+from sqlalchemy import Boolean, Column, Integer, String, DateTime
+
+from src.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    steam = Column(String, nullable=True)
+    discord = Column(String, nullable=True)
+    password = Column(String)
+    is_active = Column(Boolean, default=True)
+    data_joined = Column(DateTime, default=time.now())
