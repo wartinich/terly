a35c81402a6b0537220521fc3e95399035322f39
wartinich <vasilenko.arhip@gmail.com>
2023-04-03 08:55:33 +0300

    Include auth_router and structured references

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
 
-from src.auth.routers import router as auth_router
 from src.database import Base, engine
+
+from src.auth.routers import router as auth_router
+from src.user.routers import router as user_router
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
 app.include_router(auth_router)
+app.include_router(user_router)
