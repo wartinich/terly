fa09b30d93c9ff71e38905c89dcd38fc632dff9f
wartinich <vasilenko.arhip@gmail.com>
2023-04-04 10:12:02 +0300

    Include assistance and category router

--- src/main.py
+++ src/main.py
@@ -4,6 +4,8 @@ from src.database import Base, engine
 
 from src.auth.routers import router as auth_router
 from src.user.routers import router as user_router
+from src.assistance.routers import router as assistance_router
+from src.category.routers import router as category_router
 
 app = FastAPI()
 
@@ -11,3 +13,5 @@ Base.metadata.create_all(bind=engine)
 
 app.include_router(auth_router)
 app.include_router(user_router)
+app.include_router(assistance_router)
+app.include_router(category_router)
